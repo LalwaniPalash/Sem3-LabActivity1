c8e817d6f81fe9405c4a1fd8ea683a39f9ce751b
LalwaniPalash <147582785+LalwaniPalash@users.noreply.github.com>
2024-03-05 15:52:56 +0530

    Update 01 Palindrome Checker.py

--- 01 Palindrome Checker.py
+++ 01 Palindrome Checker.py
@@ -1,13 +1,14 @@
 import time
 
-print("Palindrome Checker!")
+print("Palindrome Checker!\n")
 time.sleep(2)
 
+def PalindromeChecker(word):
+	reversed_word = word[::-1]
+	if word == reversed_word:
+		print(f"The word ({word}) is a Palindrome.")
+	else:
+		print(f"The word ({word}) is not a Palindrome.")
+
 word = input("Enter a word: ")
-
-reversed_word = word[::-1]
-
-if word == reversed_word:
-	print(f"The word ({word}) is a Palindrome.")
-else:
-	print(f"The word ({word}) is not a Palindrome.")
+PalindromeChecker(word)
